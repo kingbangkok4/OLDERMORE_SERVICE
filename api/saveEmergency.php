50a4b5dd00eb80953728cb13cb8b7737fc1154a4
kingbangkok4 <kingbangkok4@gmail.com>
2016-12-14 22:35:56 +0700

    Merge origin/master
    Conflicts:
    	views/sellPage.php

--- api/saveEmergency.php
+++ api/saveEmergency.php
@@ -10,34 +10,47 @@ $resultArray = array();
 //$dateNow = date("d",strtotime(date("Y-m-d")));
 
 $data = array(
+    "emergency_id" => $_REQUEST["emergency_id"],
     "emergency_name" => $_REQUEST["emergency_name"],
     "emergency_mobile" => $_REQUEST["emergency_mobile"],
     "emergency_image" => $_REQUEST["emergency_image"],
     "user_id" => $_REQUEST["user_id"]
 );
-$rows = $obj->read(" emergency_name = '{$data["emergency_name"]}' ");
-if ($rows == false) {
+
+if($data["emergency_id"] == ""){
     $status = $obj->insert($data);
-}else{
-    if($data["emergency_name"] != ""){
-        $status = $obj->update(" SET emergency_name = '{$data["emergency_name"]}', "
+}
+else{
+    if($data["emergency_image"] != ""){
+        $status = $obj->update(" emergency_name = '{$data["emergency_name"]}', "
         . "emergency_mobile = '{$data["emergency_mobile"]}', "
-        . "emergency_image = '{$data["emergency_image"]}', "
-        . "user_id = '{$data["user_id"]}' ");
+        . "emergency_image = '{$data["emergency_image"]}' "
+        , " emergency_id = {$data["emergency_id"]} ");
     }else{
-        $status = $obj->update(" SET emergency_name = '{$data["emergency_name"]}', "
-        . "emergency_mobile = '{$data["emergency_mobile"]}', "
-        . "user_id = '{$data["user_id"]}' ");
+        $status = $obj->update(" emergency_name = '{$data["emergency_name"]}', " 
+        . "emergency_mobile = '{$data["emergency_mobile"]}' " 
+        , " emergency_id = {$data["emergency_id"]} ");
     }   
 }
 
+/*$rows = $obj->read(" emergency_name = '{$data["emergency_name"]}' ");
+if ($rows == false) {
+    
+}else{ 
+    
+}*/
+
 if ($status == true) {
     $arrCol = array();
-    $arrCol["status"] = "บันทึกสำเร็จ";
+    $arrCol["status"] = "1";
+    $arrCol["error"] = "บันทึกสำเร็จ";
+    // $arrCol["error"] = $status;
     array_push($resultArray,$arrCol);
 }else{
     $arrCol = array();
-    $arrCol["status"] = "บันทึกไม่สำเร็จ เกิดข้อผิดพลาด !";
+    $arrCol["status"] = "0";
+    $arrCol["error"] = "บันทึกไม่สำเร็จ เกิดข้อผิดพลาด !";
+    //$arrCol["error"] = $status;
     array_push($resultArray,$arrCol);
 }	
 echo json_encode($resultArray);
