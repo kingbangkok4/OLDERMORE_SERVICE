1f4f2e3097ef85863bee26a0f7aaa8c3773bd134
kingbangkok4 <kingbangkok4@gmail.com>
2017-01-18 17:09:50 +0700

    Merge origin/master
    Conflicts:
    	views/sellPage.php

---
+++ api/saveFavorite.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+//date_default_timezone_set('Asia/Bangkok');
+include "../lib/std.php";
+include "../lib/helper.php";
+include "../lib/dbConnector.php";
+include "../model/friend.php";
+$obj = new Friend();
+//$dateNow = date("d",strtotime(date("Y-m-d")));
+$resultArray = array();
+    $friend_id = $_REQUEST["friend_id"];
+    $status = $obj->update(" favorite = 1 ", " id = {$friend_id} ");
+    if ($status == true) {
+        $arrCol = array();
+        $arrCol["status"] = "1";
+        $arrCol["error"] = "บันทึกสำเร็จ";
+        array_push($resultArray,$arrCol);
+    }else{
+        $arrCol = array();
+        $arrCol["status"] = "0";
+        $arrCol["error"] = "บันทึกไม่สำเร็จ เกิดข้อผิดพลาด !";
+        array_push($resultArray,$arrCol);
+    }
+	
+echo json_encode($resultArray);
